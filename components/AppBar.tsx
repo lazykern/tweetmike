95b0cb769b18ea78754788aafb8202e9d18fcb42
phusitsom <phusitsom@gmail.com>
2022-07-18 14:21:25 +0700

    +| fix context path

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -7,12 +7,13 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import {useAuth} from 'contexts/AuthContext';
+import {useAuth} from '@contexts/AuthContext';
 import {useTheme} from 'next-themes';
 
 const pages = ['About'];
