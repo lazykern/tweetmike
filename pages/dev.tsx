556570e8f6318ea3150357fab9047cf2635a8eeb
phusitsom <phusitsom@gmail.com>
2022-07-20 22:30:03 +0700

    | renamed design to dev

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -4,26 +4,34 @@ import {Box, Button, Grid, Stack, TextField} from '@mui/material';
 
 import React, {useState} from 'react';
 
-export default function Design() {
+export default function Home() {
   const [data, setData] = useState<object>({});
   const [endpoint, setEndpoint] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleEndpointChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setEndpoint(e.target.value);
+    setEndpoint(e.target.value);
   };
 
   const handleSendButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    setLoading(true);
 
     if (!endpoint) {
+      setLoading(false);
       return;
     }
 
     const response = fetch('/api/twitter/' + endpoint, {
       method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
     }).then(res => res.json());
 
     response.then(res => {
+      setLoading(false);
       setData(res);
     });
   };
@@ -34,9 +42,9 @@ export default function Design() {
         <Grid item xs={16} sm={6} maxWidth={{xs: '100%', sm: '60%'}}>
           <Stack spacing={1} flex="1 1 auto">
             <Grid container spacing={1} alignItems="center">
-              <Grid item>
+              {/* <Grid item>
                 <Button> select </Button>
-              </Grid>
+              </Grid> */}
               <Grid item flexGrow={1}>
                 {' '}
                 <TextField
@@ -47,7 +55,12 @@ export default function Design() {
                 ></TextField>{' '}
               </Grid>
             </Grid>
-            <Button onClick={handleSendButton}> SEND </Button>
+            { !loading ? (
+            <Button onClick={handleSendButton}> SEND </Button>) : (
+            <Button
+            disableRipple={true}
+            > SENDING... </Button>
+            )}
             <Box color={'white'}></Box>
             <Box display={{xs: 'flex', sm: 'none'}}>
               <StyledReactJson src={data} />
